d46603ab261c3ef9f9396193b5d5f104dd648561
gingerninja412 <samdpeevor@gmail.com>
2023-08-08 19:30:10 +0100

    tidied up and finished the game

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 function getComputerChoice(){
-    let choice = Math.floor((Math.random() * 3) + 1)
-    let output = " "
+    let choice = Math.floor((Math.random() * 3) + 1);
+    let output = " ";
     if (choice == 1){
         output = "rock"
     } else if (choice == 2){
         output = "scissors"
     } else if (choice == 3){
         output = "paper"
-    }
-    return output
-}
+    };
+    return output;
+};
 
 function getPlayerChoice(){
-    let playerChoice = prompt("type S for scissors, R for rock and P for paper")
-    let output = ""
+    let playerChoice = prompt("type S for scissors, R for rock and P for paper");
+    let output = "";
     if(playerChoice == "S" || playerChoice == "s"){
         output = "scissors"
     } else if (playerChoice == "R" || playerChoice == "r"){
         output = "rock"
     } else if (playerChoice == "P" || playerChoice == "p"){
         output = "paper"
-    }
-    return output
-}
+    };
+    return output;
+};
 
 function round(computer, player){
     let win = false
     if ((computer == "paper" && player == "rock") || (computer == "rock" && player == "scissors") || (computer == "scissors" && player == "paper")){
         win = false
+        console.log("you lost, the computer picked " + computer )
     } else {
         win = true
-    }
-    return win
+        console.log("you lost, the computer picked " + computer)
+    };
+    return win;
 }
 
-console.log(getComputerChoice())
-console.log(round(getComputerChoice(), getPlayerChoice()))
+function game(){
+    let score = 0;
+    for(let i = 1; i <= 5; i++){
+        let bout = round(getComputerChoice(), getPlayerChoice())
+        if( bout == true){
+            score += 1
+        }
+    };
+    if (score >= 3){
+        return "you won"
+    }
+    return "you lost"
+};
+
+console.log(game());
