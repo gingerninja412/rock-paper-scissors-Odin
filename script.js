9c92a369898bb7269d4d6736376d49ddbc19b70e
gingerninja412 <samdpeevor@gmail.com>
2023-08-20 17:50:05 +0100

    finished game

--- script.js
+++ script.js
@@ -1,53 +1,120 @@
-function getComputerChoice(){
-    let choice = Math.floor((Math.random() * 3) + 1);
-    let output = " ";
-    if (choice == 1){
-        output = "rock"
-    } else if (choice == 2){
-        output = "scissors"
-    } else if (choice == 3){
-        output = "paper"
-    };
-    return output;
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const message = document.getElementById("anouncer");
+const history = document.getElementById("history");
+const counter = document.getElementById("round");
+const playerWin = document.getElementById("winCount");
+const computerWin = document.getElementById("loseCount");
+const restart = document.getElementById("reset")
+restart.disabled = true
+let roundCount = 1;
+let winCount = 0;
+let loseCount = 0;
+
+function restartGame() {
+    winCount = 0
+    loseCount = 0
+    roundCount = 1
+    playerWin.textContent = `${winCount}`
+    computerWin.textContent = `${loseCount}`
+    message.textContent = "Pick rock, paper, or scissors to play again"
+    counter.textContent = `Round: ${roundCount}`
+    let newEntry = document.createElement("p")
+    newEntry.textContent = "------New Round------"
+    history.appendChild(newEntry)
+    rock.disabled = false
+    paper.disabled = false
+    scissors.disabled = false
+    restart.disabled = true
 };
 
-function getPlayerChoice(){
-    let playerChoice = prompt("type S for scissors, R for rock and P for paper");
-    let output = "";
-    if(playerChoice == "S" || playerChoice == "s"){
-        output = "scissors"
-    } else if (playerChoice == "R" || playerChoice == "r"){
-        output = "rock"
-    } else if (playerChoice == "P" || playerChoice == "p"){
-        output = "paper"
+const computerTurn = () => {
+    let selection = Math.floor(Math.random() * 3) + 1;
+    if(selection == 1){
+        return "rock"
     };
-    return output;
+    if(selection == 2){
+        return "paper"
+    };
+    if(selection == 3){
+        return "scissors"
+    };
 };
 
-function round(computer, player){
-    let win = false
-    if ((computer == "paper" && player == "rock") || (computer == "rock" && player == "scissors") || (computer == "scissors" && player == "paper")){
-        win = false
-        console.log("you lost, the computer picked " + computer )
-    } else {
-        win = true
-        console.log("you lost, the computer picked " + computer)
-    };
-    return win;
-}
-
-function game(){
-    let score = 0;
-    for(let i = 1; i <= 5; i++){
-        let bout = round(getComputerChoice(), getPlayerChoice())
-        if( bout == true){
-            score += 1
-        }
-    };
-    if (score >= 3){
-        return "you won"
+const setMessages = (result) => {
+    let newEntry = document.createElement("p")
+    roundCount += 1
+    console.log(roundCount)
+    if(result == "win"){
+        message.textContent = "You won"
+        newEntry.textContent = `The player won round ${roundCount}`
+        winCount += 1
+        console.log(winCount)
+        playerWin.innerText = winCount
+    } else if(result == "lose") {
+        message.textContent = "you lost"
+        newEntry.textContent = `The computer won round ${roundCount}`
+        loseCount += 1
+        console.log(loseCount)
+        computerWin.innerText = loseCount
+    } else{
+        message.textContent = "The round was a draw"
+        newEntry.textContent = `The computer and the player drew`
     }
-    return "you lost"
+    history.appendChild(newEntry)
+    counter.textContent = `Round: ${roundCount}`
 };
 
-console.log(game());
+const compareScores = (computer, player) => {
+    if ((player == "rock" && computer == "scissors") || (player == "scissors" && computer == "paper") || (player == "paper" && computer == "rock")){
+        setMessages("win")
+    } else if(player == computer) {
+        setMessages("draw")
+    }else {
+        setMessages("lose")
+    };
+};
+
+
+
+const checkWin = () => {
+    if(winCount == 5 || loseCount == 5){
+        console.log("this works")
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        restart.disabled = false
+        if(winCount == 5){
+            message.textContent = "you won, press restart to play again"
+        } else if(loseCount == 5){
+            message.textContent = "you lose, press restart to play again"
+        }
+    }
+};
+
+restart.addEventListener("click", () => {
+    restartGame();
+})
+
+rock.addEventListener("click", () => {
+    const playerSelection = "rock";
+    const compSelection = computerTurn();
+    const winState = compareScores(compSelection, playerSelection);
+    checkWin()
+});
+
+paper.addEventListener("click", () => {
+    const playerSelection = "paper";
+    const compSelection = computerTurn();
+    const winState = compareScores(compSelection, playerSelection);
+    checkWin()
+})
+
+scissors.addEventListener("click", () => {
+    const playerSelection = "scissors";
+    const compSelection = computerTurn();
+    const winState = compareScores(compSelection, playerSelection);
+    checkWin()
+})
+
